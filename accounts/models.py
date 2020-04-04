5a7940b2e971cfb096845190d5526aa531b1af75
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-04 12:05:46 -0500

    rmv something that was copied

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,6 @@ class Account(models.Model):
     def __str__(self):
         return self.user.username
 
-@receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
