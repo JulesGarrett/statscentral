40bcfb78ab210589e26c29633411d62f1fa61f73
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-06 23:09:01 -0500

    add cities html

--- cities/views.py
+++ cities/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home_screen_view(request):
     print(request.headers)
-    return render(request, "base.html", {})
+    return render(request, "cities/home.html", {})
