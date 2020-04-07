d6afac302216f63105158f7b216e1c2bc8f5e056
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-07 00:22:01 -0500

    rmv account

--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.account, name='account'),
-    path('edit', views.account_edit, name='account_edit'),
-
-]
