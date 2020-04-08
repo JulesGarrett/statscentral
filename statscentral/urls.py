d99fea069b96101b57d52dedfcef10c199d37819
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-07 21:32:41 -0500

    add logout

--- statscentral/urls.py
+++ statscentral/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from cities.views import home_screen_view
-from account.views import registration_view
+from account.views import registration_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
     path('register/', registration_view, name="register"),
+    path('logout/', logout_view, name="logout"),
 ]
