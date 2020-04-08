71df37ce710ff4f80dba8d26259f15f8210ba7ce
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-08 01:07:56 -0500

    fix typo

--- statscentral/urls.py
+++ statscentral/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('register/', registration_view, name="register"),
     path('logout/', logout_view, name="logout"),
     path('login/', login_view, name="login"),
-    path('account/', account_view, name="login"),
+    path('account/', account_view, name="account"),
 
 ]
