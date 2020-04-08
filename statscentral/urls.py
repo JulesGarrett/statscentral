8b1e444b0b8a70c3ddaa1a999c48ed87bc6dd1e0
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-08 01:06:35 -0500

    add bootstraps

--- statscentral/urls.py
+++ statscentral/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from cities.views import home_screen_view
-from account.views import registration_view, logout_view, login_view
+from account.views import registration_view, logout_view, login_view, account_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +25,6 @@ urlpatterns = [
     path('register/', registration_view, name="register"),
     path('logout/', logout_view, name="logout"),
     path('login/', login_view, name="login"),
+    path('account/', account_view, name="login"),
 
 ]
