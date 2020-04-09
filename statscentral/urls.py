6ee00e7c799a8a422523449812a996847938a605
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-08 22:52:07 -0500

    fix typo

--- statscentral/urls.py
+++ statscentral/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
 
      # Password reset
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
-        name='password_change_complete'),
+        name='password_change_done'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
         name='password_change'),
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
