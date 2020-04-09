6bceac07444b505c77bc487d9f8f6f9fba467266
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-08 22:49:08 -0500

    add password reset

--- statscentral/urls.py
+++ statscentral/urls.py
@@ -27,4 +27,16 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('account/', account_view, name="account"),
 
+     # Password reset
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+        name='password_change_complete'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+        name='password_change'),
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+     name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+     name='password_reset_complete'),
+
 ]
