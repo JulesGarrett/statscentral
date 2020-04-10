ce16bde59e7e87de789a93e6c795b91309a95907
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-10 01:30:36 -0500

    fix wrong linking

--- cities/views.py
+++ cities/views.py
@@ -6,7 +6,7 @@ def home_screen_view(request):
     context = {}
     return render(request, "cities/home.html", context)
 
-def search_cities(text):
+def search_cities(request):
     context = {}
     context['cities'] = ["Kanas City", "New York", "Boston"]
     return render(request, 'cities/search.html', context)
