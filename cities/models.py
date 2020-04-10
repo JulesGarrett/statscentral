b82ee9bb5ec852a21f45fb045490f60ecc387270
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-09 21:33:09 -0500

    change city model

--- cities/models.py
+++ cities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Cities(models.Model):
-	State_ID 				= models.IntegerField(blank=True, null=True, primary_key=True)
+	State_ID 				= models.IntegerField(blank=True, null=True)
 	City 					= models.CharField(max_length=50, null=False, blank=False, primary_key=True)
 	State		 			= models.CharField(max_length=50, null=False, blank=False)
 
