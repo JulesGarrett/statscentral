a53d28acb8694ed6dd9cfb318e2a606ea83fb922
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-09 21:31:35 -0500

    add cities model

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Cities(models.Model):
+	State_ID 				= models.IntegerField(blank=True, null=True, primary_key=True)
+	City 					= models.CharField(max_length=50, null=False, blank=False, primary_key=True)
+	State		 			= models.CharField(max_length=50, null=False, blank=False)
+
+	def __str__(self):
+		return self.City
