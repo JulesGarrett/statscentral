7a4eadc89ab7be4108a64c939fa4bdeb91c282f5
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 14:31:08 -0500

    change form

--- statscentral/settings.py
+++ statscentral/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
