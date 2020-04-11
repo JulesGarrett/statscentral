850e7c4047dbe1d1059bf123aa4d2b6cc28c92c7
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 00:06:01 -0500

    name col in sql

--- cities/views.py
+++ cities/views.py
@@ -22,6 +22,6 @@ def search_cities(request):
 
 def get_cities_sql():
     with connection.cursor() as cursor:
-        cursor.execute("SELECT City, Sum(Population) FROM `cities_cities` group by City Limit 10;")
+        cursor.execute("SELECT City, Sum(Population) AS Population FROM `cities_cities` group by City Limit 10;")
         cites = dictfetchall(cursor)	#[{'Game_ID': 1, 'Description': "...", Image:"...", ...}, {'Game_ID': 2, 'Description': "...", Image:"..."}...]
     return cites
