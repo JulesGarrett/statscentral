5912a2d4fee7888e7e50b80e2ad748b25eb4506a
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 12:54:29 -0500

    forgot imports

--- cities/models.py
+++ cities/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+from django.conf import settings
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
 
 class Cities(models.Model):
 	Zip_ID 					= models.IntegerField(blank=True, null=True)
