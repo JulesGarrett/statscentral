a0f827fcf6e83c27675c0fa1d6d5099cffa36e06
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 13:43:50 -0500

    trying to get options to work

--- cities/models.py
+++ cities/models.py
@@ -28,7 +28,7 @@ class CityReviews(models.Model):
 	date_updated 			= models.DateTimeField(auto_now=True, verbose_name="date updated")
 	author 					= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	slug 					= models.SlugField(blank=True, unique=True)
-
+	# give options 
 	OVERALLCITY = 1
     SCHOOL = 2
     MILITARY = 3
