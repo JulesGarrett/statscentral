e5b06ef5e61906aba230c59d334d6a7da86d430b
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 00:50:39 -0500

    typo

--- cities/urls.py
+++ cities/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from cities.views import search_cities, city_details
 
-app_name = 'blog'
+app_name = 'cities'
 
 urlpatterns = [
     path('search', search_cities, name="search"),
-    path('city_detail', city_details, name="city_details")
+    path('city_details', city_details, name="city_details")
  ]
