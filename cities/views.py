1650b4d8a50750cbd4df5b580e23f32c53a43366
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 00:59:46 -0500

    change detail view

--- cities/views.py
+++ cities/views.py
@@ -11,14 +11,17 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 
+
 def home_screen_view(request):
     context = {}
     return render(request, "cities/home.html", context)
+
 
 def search_cities(request):
     context = {}
     context['cities'] = get_cities_sql()
     return render(request, 'cities/search.html', context)
+
 
 def get_cities_sql():
     with connection.cursor() as cursor:
@@ -26,10 +29,19 @@ def get_cities_sql():
         cites = dictfetchall(cursor)
     return cites
 
+
+def get_one_city(city_name):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT City, Sum(Population) AS Population FROM `cities_cities` WHERE City = "+str(city_name)+" group by City Limit 10;")
+        cites = dictfetchall(cursor)
+    return cites
+
+
 def city_details(request, city_name):
     context = {}
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT City, Sum(Population) AS Population FROM `cities_cities` group by City Having City = "+str(city_name))
-        city = dictfetchall(cursor)
-        context['city'] = city
-    return render(request, 'cities/city_details.html', context)
+    city = get_one_city(city_name)
+    if not city:
+        raise Http404("City does not exist")
+    context['city'] = city
+    context['user'] = request.user
+    return render(request, "cities/city_details.html", context)
