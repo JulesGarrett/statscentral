85af573cffc1c378118a4d5493af1b2f5809da16
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 15:23:32 -0500

    fix spacing

--- cities/views.py
+++ cities/views.py
@@ -55,7 +55,7 @@ def create_review(request):
     form = CreateReviewForm(request.POST or None)
     if form.is_valid():
         obj = form.save(commit=False)
-		author = Account.objects.filter(email=request.user.email).first()
+        author = Account.objects.filter(email=request.user.email).first()
 		obj.author = author
 		obj.save()
         form = CreateReviewForm()
