fa254c1e6eff56fde2d1dee5ff8170825a20f523
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 19:01:30 -0500

    hat spcing

--- cities/forms.py
+++ cities/forms.py
@@ -15,11 +15,11 @@ class UpdateReviewForm(forms.ModelForm):
 		fields = ['title', 'body', 'image']
 
 	def save(self, commit=True):
-		review = self.instance
-		review.City = self.cleaned_data['City']
-		review.Comments = self.cleaned_data['Comments']
+        review = self.instance
+        review.City = self.cleaned_data['City']
+        review.Comments = self.cleaned_data['Comments']
         review.Rating = self.cleaned_data['Rating']
 
-		if commit:
-			review.save()
-		return review
+        if commit:
+            review.save()
+            return review
