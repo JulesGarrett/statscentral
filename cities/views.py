9ef68073362ce24bd4eeab2d5b9d1756c6769f8e
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 19:22:40 -0500

    forgot import

--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db import connection
 # from cities.models import Cities, Reviews
 from cities.models import CityReviews
