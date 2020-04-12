3671a9a0270beaadca6934d4dc6bb8948eabe209
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 20:13:24 -0500

    going back

--- cities/views.py
+++ cities/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db import connection
 # from cities.models import Cities, Reviews
-from cities.models import CityReviews
+from cities.models import Reviews
 from cities.forms import CreateReviewForm, UpdateReviewForm
 from account.models import Account
 
@@ -27,6 +27,16 @@ def get_cities_sql():
 def search_city_match(query=None):
     with connection.cursor() as cursor:
         cursor.execute("SELECT City, Sum(Population) AS Population FROM `cities_cities` WHERE City LIKE '%" +str(query)+ "%' group by City Limit 10")
+        cities = dictfetchall(cursor)
+    return cities
+
+def insert_review(form, username):
+    city = form.City
+    comments = form.Comments
+    rating = form.rating
+
+    with connection.cursor() as cursor:
+        cursor.execute('INSERT INTO cities_reviews (City, Comments, Rating, author, slug) VALUES ("'+str(city)+'", "'+str(comments)+'", '+str(rating)+', "'+str(username)+'", "'+str(city)+str(username)+'");')
         cities = dictfetchall(cursor)
     return cities
 
@@ -57,8 +67,7 @@ def create_review(request):
 	form = CreateReviewForm(request.POST or None)
 	if form.is_valid():
 		obj = form.save(commit=False)
-		author = Account.objects.filter(email=request.user.email).first()
-		obj.author = author
+		obj.author = request.user.username
 		obj.save()
 
 	context['form'] = form
@@ -67,7 +76,7 @@ def create_review(request):
 
 def detail_review(request, slug):
     context = {}
-    review = get_object_or_404(CityReviews, slug=slug)
+    review = get_object_or_404(Reviews, slug=slug)
     context['review'] = review
 
     return render(request, 'cities/detail_review.html', context)
@@ -78,7 +87,7 @@ def edit_review(request, slug):
 	context = {}
 	user = request.user
 
-	review = get_object_or_404(CityReviews, slug=slug)
+	review = get_object_or_404(Reviews, slug=slug)
 	if request.POST:
 		form = UpdateReviewForm(request.POST or None, request.FILES or None, instance=review)
 		if form.is_valid():
