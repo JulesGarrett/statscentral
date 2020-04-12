8327da11239a3648324688895d13826f2b14a2ea
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 19:07:42 -0500

    add slug

--- cities/urls.py
+++ cities/urls.py
@@ -6,5 +6,5 @@ app_name = 'cities'
 urlpatterns = [
     path('search', search_cities, name="search"),
     path('create_review', create_review, name="create_review"),
-    path('edit_review', edit_review, name="edit_review")
+    path('<slug>/edit/', edit_review, name="edit_review")
  ]
