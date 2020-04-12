6fec6b7fa1916716e87da447ed05763fff14f0f6
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-11 22:11:59 -0500

    idk

--- cities/views.py
+++ cities/views.py
@@ -88,7 +88,7 @@ def edit_review(request, slug):
 
     review = get_object_or_404(CityReviews, slug=slug)
     if request.POST:
-        form = UpdateReviewForm(request.POST, instance=review)
+        form = UpdateReviewForm(request.POST or None, instance=review)
         if form.is_valid():
             obj = form.save(commit=False)
             obj.save()
