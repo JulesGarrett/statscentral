e9cabf1afbfcb63fc1bfddd8d37da27dd6c6be05
Jules Garrett <jrgarrett3222@gmail.com>
2020-04-28 21:11:03 -0500

    edit form

--- cities/forms.py
+++ cities/forms.py
@@ -12,11 +12,10 @@ class UpdateReviewForm(forms.ModelForm):
 
     class Meta:
         model = CityReviews
-        fields = ['City','Comments', 'Rating']
+        fields = ['Comments', 'Rating']
 
     def save(self, commit=True):
         review = self.instance
-        review.City = self.cleaned_data['City']
         review.Comments = self.cleaned_data['Comments']
         review.Rating = self.cleaned_data['Rating']
 
